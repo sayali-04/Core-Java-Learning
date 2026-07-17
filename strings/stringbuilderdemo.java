e120a397027c0a0cc10fbcd577e923079bb17295
Sayali Pawar <94274989+sayali-04@users.noreply.github.com>
2026-07-17 10:24:56 +0530

    Update stringbuilderdemo.java

--- strings/stringbuilderdemo.java
+++ strings/stringbuilderdemo.java
@@ -10,34 +10,33 @@ public class stringbuilderdemo {
         //6 + 5 + 8= 19--- greater than inital capacity so its doubles 16*2+2=34
 
         // //insert
-        // sb.insert(2, 'i'); //SaiyaliPawar
-
-        // System.out.println(sb);
+         sb.insert(2, 'i'); //SaiyaliPawar
+         System.out.println(sb);
 
         // //delete
-        // sb.delete(0, 2);
-        // // System.out.println(sb); //iyaliPawar
+         sb.delete(0, 2);
+         System.out.println(sb); //iyaliPawar
 
         // //deletecharAt
-        // sb.deleteCharAt(1); //ialiPawar
-        // System.out.println(sb);
+        sb.deleteCharAt(1); //ialiPawar
+        System.out.println(sb);
 
         //replace
-       // sb.replace(1, 3,"XY");
-        // System.out.println(sb);
+        sb.replace(1, 3,"XY");
+        System.out.println(sb);
 
-       /// sb.reverse();
-       // System.out.println(sb);
+        sb.reverse();
+        System.out.println(sb);
 
-        // sb.charAt(1);
-        // sb.setCharAt(1,'P');
+        sb.charAt(1);
+        sb.setCharAt(1,'P');
 
-    //  System.out.println(sb.length());
+       System.out.println(sb.length());
 
-    // System.out.println(sb.capacity());
+       System.out.println(sb.capacity());
 
-    //  sb.ensureCapacity(100);
-    System.out.println(sb.capacity());
+        sb.ensureCapacity(100);
+       System.out.println(sb.capacity());
 
        sb.trimToSize();
        System.out.println(sb.capacity());

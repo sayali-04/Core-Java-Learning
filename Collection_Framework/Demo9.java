3c6a7c81c9a4e495d81114b8fcf1b0211bb7d699
Sayali Pawar <sayalipawar1934@gmail.com>
2026-08-05 23:15:53 +0530

    Hashmap and treemap practice

---
+++ Collection_Framework/Demo9.java
@@ -0,0 +1,32 @@
+package Collection_Framework;
+
+import java.util.*;
+
+public class Demo9 {
+
+     public static void main(String[] args) {
+    //HashMap and LinkedHashMap
+
+    // HashMap<Integer,String>map1=new HashMap<>();
+
+    // HashMap<Integer,String>mp2=new HashMap<>(100);
+    
+    // HashMap<Integer,String>mp3=new HashMap<>(100,0.8f);
+
+    // HashMap<Integer,String> mp4=new HashMap<>(mp3);
+
+    //TreeMap methods
+
+    TreeMap<Integer,String> map =new TreeMap<>();
+    map.put(101,"Sayali");
+    map.put(101,"Aditya");
+    map.put(101,"Priya");
+    
+
+
+
+
+
+     }
+
+}

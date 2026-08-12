06050a577c6c05a89139b4b46b06162a274bb11f
Sayali Pawar <sayalipawar1934@gmail.com>
2026-08-12 11:41:23 +0530

    Added Stack implementation in collection framework

---
+++ Collection_Framework/Demo11.java
@@ -0,0 +1,19 @@
+package Collection_Framework;
+
+import java.util.*;
+
+//Stack using collection framework
+public class Demo11 {
+    public static void main(String[] args) {
+        Stack<Integer>s=new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+
+    }
+}
